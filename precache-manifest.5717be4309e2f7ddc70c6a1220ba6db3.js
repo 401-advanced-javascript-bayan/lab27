72b748f4f76bc7d4807015ad759fd47c9307a36d
bayanmhmd <runner@fv-az82.kgpmdgro43xuzay55ewsjmxkfa.bx.internal.cloudapp.net>
2020-02-25 15:09:14 +0000

    Updates

---
+++ precache-manifest.5717be4309e2f7ddc70c6a1220ba6db3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba793b669b1f646c8bb22c6f2047851",
+    "url": "/401-advanced-javascript-bayan/lab27/index.html"
+  },
+  {
+    "revision": "0b5571a18678e5d0f764",
+    "url": "/401-advanced-javascript-bayan/lab27/static/css/main.fb354af3.chunk.css"
+  },
+  {
+    "revision": "749eec1199943858d6b1",
+    "url": "/401-advanced-javascript-bayan/lab27/static/js/2.344b3e7f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/401-advanced-javascript-bayan/lab27/static/js/2.344b3e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b5571a18678e5d0f764",
+    "url": "/401-advanced-javascript-bayan/lab27/static/js/main.e2ea6d32.chunk.js"
+  },
+  {
+    "revision": "18da0782717903705174",
+    "url": "/401-advanced-javascript-bayan/lab27/static/js/runtime-main.246d07ee.js"
+  }
+]);
