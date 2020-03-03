5921fec69cf98aa75928c853805225ef14a32855
bayan <bayana.mhmd99@gmail.com>
2020-03-03 15:43:44 +0200

    done

--- src/__test__/components/counter/counter.test.js
+++ src/__test__/components/counter/counter.test.js
@@ -6,31 +6,37 @@
 /* eslint-disable semi */
 // import React from 'react';
 // import Enzyme, { shallow, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import Adapter from 'enzyme-adapter-react-16';
 // import renderer from 'react-test-renderer';
 // import Counter from '../../../components/counter.js'
-// Enzyme.configure({ adapter: new Adapter() });
+
 import React from 'react';
 import { shallow, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
 import Counter from '../../../components/counter.js';
 
+
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
+
 describe('<Counter/>', () => {
   it('exists at the start of our application', () => {
-    // let app = shallow(<Counter />);
-    // expect(app.find('h3').exists()).toBeTruthy();
+    let app = shallow(<Counter />);
+    expect(app.find('h3').exists()).toBeTruthy();
   });
 
   it('changes the state on click', ()=> {
-    // let app = mount(<Counter />);
-    // let button = app.find('.one');
-    // button.simulate('click');
-    // expect(app.state('count')).toBeTruthy();
+    let app = mount(<Counter />);
+    let button = app.find('.one');
+    button.simulate('click');
+    expect(app.state('ctr')).toBeTruthy();
     // expect(app.find('span').text()).toContain('false');
   });
 
   it('renders correctly', () => {
-    // const tree = renderer.create(<Counter />).toJSON();
-    // expect(tree).toMatchSnapshot();
+    const tree = renderer.create(<Counter />).toJSON();
+    expect(tree).toMatchSnapshot();
   })
 });
